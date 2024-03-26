64b34398e8b01dfcfc51ad85bb6840b1ce7dd43d
Jonatan <jsantanamay@gmail.com>
2024-03-26 14:35:30 -0600

    feat: creando entidad ruta (DevOpsBackend-006)

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,86 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  afterEach(async () => {
+    await app.close();
+  });
+
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('/rutas (POST)', () => {
+    const rutaData = {
+      NombreRuta: 'string',
+      FechaCreacionRuta: new Date(),
+      EstadoRuta: true,
+      LatitudInicio: 0,
+      LongitudInicio: 0,
+      LatitudDestino: 0,
+      LongitudDestino: 0,
+      deletedAt: new Date(),
+    };
+
+    return request(app.getHttpServer())
+    .post('/rutas')
+    .send(rutaData)
+    .expect(201);
+
+  });
+
+  it('/rutas (DELETE)', async () => {
+    const rutaData = {
+      NombreRuta: 'string',
+      FechaCreacionRuta: new Date(),
+      EstadoRuta: true,
+      LatitudInicio: 0,
+      LongitudInicio: 0,
+      LatitudDestino: 0,
+      LongitudDestino: 0
+    };
+
+    const response = await request(app.getHttpServer())
+    .post('/rutas')
+    .send(rutaData)
+    .expect(201);
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    const createdRutaId = response.body.id;
+    
+    return request(app.getHttpServer())
+      .delete(`/rutas/${createdRutaId}`)
+      .expect(200);
+  });
+
+  it('/rutas (PUT)', async () => {
+    const rutaData = {
+      NombreRuta: 'string',
+      FechaCreacionRuta: new Date(),
+      EstadoRuta: true,
+      LatitudInicio: 0,
+      LongitudInicio: 0,
+      LatitudDestino: 0,
+      LongitudDestino: 0
+    };
+  
+    const createResponse = await request(app.getHttpServer())
+      .post('/rutas')
+      .send(rutaData)
+      .expect(201);
+  
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  
+    const createdRutaId = createResponse.body.id;
+  
+    const updateResponse = await request(app.getHttpServer())
+      .put(`/rutas/${createdRutaId}`)
+      .send({ EstadoRuta: false }) 
+      .expect(200);
+    console.log(updateResponse.body);
+    
+    expect(updateResponse.body.EstadoRuta).toBe(false);
   });
 });
