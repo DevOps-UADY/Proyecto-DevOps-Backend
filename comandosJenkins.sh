b94022a473646155a8804a7863047567bbaae192
Jonatan <a17000799@alumnos.uayd.mx>
2024-03-29 23:45:11 -0600

    fix: cambio de dockerfile (DevOpsBackend-008)

---
+++ comandosJenkins.sh
@@ -0,0 +1,20 @@
+npm install
+RAMA=$(echo $GIT_BRANCH | cut -b 8-14 | tr '[:upper:]' '[:lower:]' | tr '/' '_')
+
+docker build -t proyectodevopsbackend-$RAMA:1.0.0-$BUILD_NUMBER .
+
+# Obtener el ID del contenedor
+CONTAINER_ID=$(docker ps -a -q --filter="name=proyectodevopsbackend-$RAMA")
+
+# Verificar si el contenedor existe
+if [ -n "$CONTAINER_ID" ]; then
+    # Detener el contenedor
+    docker stop "$CONTAINER_ID"
+    
+    # Eliminar el contenedor
+    docker rm "$CONTAINER_ID"
+else
+    echo "El contenedor no existe."
+fi
+
+docker run -d --name proyectodevopsbackend-$RAMA -p 3000:3000 proyectodevopsbackend-$RAMA:1.0.0-$BUILD_NUMBER
--- dockerfile
+++ dockerfile
@@ -5,4 +5,4 @@ COPY .env.development.example /app/.env.development
 RUN npm install
 COPY . .
 EXPOSE 3000
-CMD ["npm", "run", "start:prod"]
+CMD ["./start.sh"]
