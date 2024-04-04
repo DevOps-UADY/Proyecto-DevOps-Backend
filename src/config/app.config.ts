e84a27f4c7f9f73eb836b91046732c87a349582e
Jonatan <jsantanamay@gmail.com>
2024-04-04 06:23:05 -0600

    fix: cambiando la variable host del .env.developmet.example (DevOpsBackend-008)

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: '.env.development' });
 
 export default () => ({
-    host: '172.19.0.2',
+    host: process.env.MYSQL_HOST || '172.19.0.2',
     port: 3306,
     username: process.env.MYSQL_USERNAME || 'root',
     password: process.env.MYSQL_ROOT_PASSWORD || '',
