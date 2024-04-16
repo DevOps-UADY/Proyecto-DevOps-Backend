01709ddb62e882e88b4c6478f3040dc8cd67b6ea
Jonatan <a17000799@alumnos.uayd.mx>
2024-04-16 11:16:10 -0600

    fix: packege (DevOpsBackend-001)

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: '.env.development' });
 
 export default () => ({
-    host: process.env.POSTGRES_HOST || '172.17.0.2',
+    host: process.env.POSTGRES_HOST || '172.17.0.2', 
     port: parseInt(process.env.POSTGRES_PORT) || 5432,
     username: process.env.POSTGRES_USER || 'root',
     password: process.env.POSTGRES_PASSWORD || '',
