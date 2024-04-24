12f386728225a6f0a0804aaf641c2b85fe453077
Jonatan <a17000799@alumnos.uayd.mx>
2024-04-24 00:23:03 -0600

    feat: creando unit test (DevOpsBackend-013)

--- src/rutas/rutas.controller.spec.ts
+++ src/rutas/rutas.controller.spec.ts
@@ -1,65 +1,149 @@
-import { Test } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
 import { RutasController } from './rutas.controller';
 import { RutasService } from './rutas.service';
 
 describe('RutasController', () => {
   let rutasController: RutasController;
-  let rutasService: RutasService;
+
+  const mockRutaService = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
 
   beforeEach(async () => {
-    const moduleRef = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [RutasController],
       providers: [
         {
           provide: RutasService,
-          useValue: {
-            findAll: jest.fn().mockResolvedValue([]),
-          },
+          useValue: mockRutaService,
         },
       ],
     }).compile();
 
-    rutasService = moduleRef.get<RutasService>(RutasService);
-    rutasController = moduleRef.get<RutasController>(RutasController);
+    rutasController = module.get<RutasController>(RutasController);
   });
 
-  describe('findAll', () => {
-    it('should return an empty array', async () => {
-      const result = await rutasController.findAll();
-      expect(result).toEqual([]);
-    });
+  it('should be defined', () => {
+    expect(rutasController).toBeDefined();
   });
 
-  describe('otherTest', () => {
-    it('should return a specific array', async () => {
-      jest.spyOn(rutasService, 'findAll').mockResolvedValue([
-        {
-          "id": 0,
-          "NombreRuta": "string",
-          "FechaCreacionRuta": "2024-04-04",
-          "EstadoRuta": true,
-          "LatitudInicio": 0,
-          "LongitudInicio": 0,
-          "LatitudDestino": 0,
-          "LongitudDestino": 0,
-          "deletedAt": new Date("2024-04-16T03:32:14.053Z")
-        }
-      ]);
+  it('create => shuld create a new ruta by a given data', async () =>{
+    const rutaDTO = {
+      NombreRuta: "San Nicolas del Sur",
+      FechaCreacionRuta: "2024-04-04",
+      EstadoRuta: true,
+      LatitudInicio: 80,
+      LongitudInicio: 170,
+      LatitudDestino: 80,
+      LongitudDestino: 170
+    };
 
-      const result = await rutasController.findAll();
-      expect(result).toEqual([
-        {
-          "id": 0,
-          "NombreRuta": "string",
-          "FechaCreacionRuta": "2024-04-04",
-          "EstadoRuta": true,
-          "LatitudInicio": 0,
-          "LongitudInicio": 0,
-          "LatitudDestino": 0,
-          "LongitudDestino": 0,
-          "deletedAt": new Date("2024-04-16T03:32:14.053Z")
-        }
-      ]);
-    });
+    const ruta = {
+      id: 1,
+      NombreRuta: "San Nicolas del Sur",
+      FechaCreacionRuta: "2024-04-04",
+      EstadoRuta: true,
+      LatitudInicio: 80,
+      LongitudInicio: 170,
+      LatitudDestino: 80,
+      LongitudDestino: 170
+    };
+
+    jest.spyOn(mockRutaService, 'create').mockResolvedValue(ruta);
+    const result = await rutasController.create(rutaDTO);
+    expect(mockRutaService.create).toHaveBeenCalled();
+    expect(mockRutaService.create).toHaveBeenCalledWith(rutaDTO);
+    expect(result).toEqual(ruta);
   });
+
+  it('findAll => should return an array of rutas', async () =>{
+    const rutas = [
+      {
+        id: 1,
+        NombreRuta: "San Nicolas del Sur",
+        FechaCreacionRuta: "2024-04-04",
+        EstadoRuta: true,
+        LatitudInicio: 80,
+        LongitudInicio: 170,
+        LatitudDestino: 80,
+        LongitudDestino: 170
+      },
+      {
+        id: 2,
+        NombreRuta: "San Nicolas del Norte",
+        FechaCreacionRuta: "2024-04-04",
+        EstadoRuta: true,
+        LatitudInicio: 80,
+        LongitudInicio: 170,
+        LatitudDestino: 80,
+        LongitudDestino: 170
+      }
+    ];
+
+    jest.spyOn(mockRutaService, 'findAll').mockResolvedValue(rutas);
+    const result = await rutasController.findAll();
+    expect(mockRutaService.findAll).toHaveBeenCalled();
+    expect(result).toEqual(rutas);
+  });
+
+  it('findOne => should return a specific ruta by a given id', async () =>{
+    const ruta = {
+      id: 1,
+      NombreRuta: "San Nicolas del Sur",
+      FechaCreacionRuta: "2024-04-04",
+      EstadoRuta: true,
+      LatitudInicio: 80,
+      LongitudInicio: 170,
+      LatitudDestino: 80,
+      LongitudDestino: 170
+    };
+
+    jest.spyOn(mockRutaService, 'findOne').mockResolvedValue(ruta);
+    const result = await rutasController.findOne(1);
+    expect(mockRutaService.findOne).toHaveBeenCalled();
+    expect(mockRutaService.findOne).toHaveBeenCalledWith(1);
+    expect(result).toEqual(ruta);
+  });
+
+  it('update => should update a specific ruta by a given id and data', async () =>{
+    const rutaDTO = {
+      NombreRuta: "San Nicolas del Sur",
+      FechaCreacionRuta: "2024-04-04",
+      EstadoRuta: true,
+      LatitudInicio: 80,
+      LongitudInicio: 170,
+      LatitudDestino: 80,
+      LongitudDestino: 170
+    };
+
+    const ruta = {
+      id: 1,
+      NombreRuta: "San Nicolas del Sur",
+      FechaCreacionRuta: "2024-04-04",
+      EstadoRuta: true,
+      LatitudInicio: 80,
+      LongitudInicio: 170,
+      LatitudDestino: 80,
+      LongitudDestino: 170
+    };
+
+    jest.spyOn(mockRutaService, 'update').mockResolvedValue(ruta);
+    const result = await rutasController.update(1, rutaDTO);
+    expect(mockRutaService.update).toHaveBeenCalled();
+    expect(mockRutaService.update).toHaveBeenCalledWith(1, rutaDTO);
+    expect(result).toEqual(ruta);
+  });
+
+  it('remove => should delete a specific ruta by a given id', async () =>{
+    jest.spyOn(mockRutaService, 'remove').mockResolvedValue(true);
+    const result = await rutasController.remove(1);
+    expect(mockRutaService.remove).toHaveBeenCalled();
+    expect(mockRutaService.remove).toHaveBeenCalledWith(1);
+    expect(result).toEqual(true);
+  });
+
 });
