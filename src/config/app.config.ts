46ef115b6041406585ce52584890d334708ba186
Jonatan <a17000799@alumnos.uayd.mx>
2024-05-23 02:28:36 -0600

    fix: reparando logstash (DevOpsBackend-020)

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: '.env.development' });
 
 export default () => ({
-    host: process.env.POSTGRES_HOST || '172.22.0.4', 
+    host: '172.22.0.4', 
     port: parseInt(process.env.POSTGRES_PORT) || 5432,
     username: process.env.POSTGRES_USER || 'root',
     password: process.env.POSTGRES_PASSWORD || '',
