f673db127836e2d22274a1d94f89c4f7f757f925
Ariel Fernández <88345754+ArielFdz@users.noreply.github.com>
2024-05-27 07:59:29 -0600

    feat: agregando logger a los vehículos (DevOpsBackend-019)
    feat: agregando logger a los vehiculos (DevOpsBackend-019)

--- src/vehiculos/vehiculos.controller.ts
+++ src/vehiculos/vehiculos.controller.ts
@@ -4,30 +4,39 @@ import { CreateVehiculoDto } from './dto/create-vehiculo.dto';
 import { UpdateVehiculoDto } from './dto/update-vehiculo.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { FormDataRequest } from 'nestjs-form-data';
+import { AppLogger } from '../logger/logger.service';
+
 @ApiTags('vehiculos')
 @Controller('vehiculos')
 export class VehiculosController {
-  constructor (private readonly vehiculosService: VehiculosService) {}
+  constructor (
+    private readonly vehiculosService: VehiculosService,
+    private readonly logger: AppLogger
+  ) {}
 
   @FormDataRequest()
   @Post()
   @HttpCode(201)
   create (@Body() createVehiculoDto: CreateVehiculoDto) {
+    this.logger.log('Llamando POST /vehiculos');
     return this.vehiculosService.create(createVehiculoDto);
   }
 
   @Get()
   findAll () {
+    this.logger.log('Llamando GET /vehiculos');
     return this.vehiculosService.findAll();
   }
 
   @Get('total')
   findTotal () {
+    this.logger.log('Llamando GET /totalVehiculos');
     return this.vehiculosService.findTotal();
   }
 
   @Get(':id')
   findOne (@Param('id') id: number) {
+    this.logger.log('Llamando GET /vehiculos/:id');
     return this.vehiculosService.findOne(id);
   }
 
@@ -35,12 +44,14 @@ export class VehiculosController {
   @FormDataRequest()
 
   async update (@Param('id') id: number, @Body() updateVehiculoDto: UpdateVehiculoDto) {
+    this.logger.log('Llamando PUT /vehiculos/:id');
     return await this.vehiculosService.update(id, updateVehiculoDto);
   }
 
   @Delete(':id')
   
   remove (@Param('id') id: number) {
+    this.logger.log('Llamando DELETE /vehiculos/:id');
     return this.vehiculosService.remove(id);
   }
 }
